188ce8ad4648e95759ab8f8a86581c21bd033a7d
Vitalii Drahun <vdragun90@gmail.com>
2022-05-28 17:56:05 +0300

    Add Spring Boot skeleton

---
+++ src/test/java/org/vdrahun/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vdrahun.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
